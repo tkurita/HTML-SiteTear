09a58fc529048e9b1f71972c5130e57b8cc47e32
Tetsuro Kurita <tkurita@mac.com>
2009-05-17 09:20:04 +0000

    [HTML-SiteTear] version 1.4.4

--- lib/HTML/SiteTear.pm
+++ lib/HTML/SiteTear.pm
@@ -27,11 +27,11 @@ HTML::SiteTear - Make a separated copy of a part of the site
 
 =head1 VERSION
 
-Version 1.43
+Version 1.44
 
 =cut
 
-our $VERSION = '1.43';
+our $VERSION = '1.44';
 
 =head1 SYMPOSIS
 
